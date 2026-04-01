72a46a55137fb2a3ca2b74e5155ba380d67c9d47
Nigel Nindo <nigelnindodev@gmail.com>
2026-04-01 12:25:56 +0300

    Add solution for challenge 1

---
+++ challenges/01-counter/solution.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter </h1>
+      < p > Count: {count} </p>
+      < button onClick={() => setCount(prevCount => Math.max(0, prevCount - 1))
+      }> -</button>
+      < button onClick={() => setCount(prevCount => prevCount + 1)}> +</button>
+    </div>
+  );
+}
+
+export default App;
